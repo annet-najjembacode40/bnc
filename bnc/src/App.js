0ede9026d72ffe03b0d852220d81ae1d7a7b391b
tclay <tclay@snapchat.com>
2021-01-19 15:33:25 -0800

    Created a feed with a single video inside

--- bnc/src/App.js
+++ bnc/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Feed from './components/feed';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hei
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Container className = "my-auto">
+     <Feed />
+  </Container>
   );
 }
 
